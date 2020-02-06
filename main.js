d0decde3f24c038e4d669fb5812dd578c4370c60
SnowLeopardMoonBear <mindflo@korea.ac.kr>
2020-02-06 19:18:50 +0900

    10강까지
    쿼리 스트링 읽기

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 var http = require('http');
 var fs = require('fs');
-var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-      url = '/index.html';
+var url = require('url'); //node.js에서 url 모듈을 사용, 앞으로 url이란 변수로 접근해 사용.
+
+
+var app = http.createServer(function(request,response){ //익명(위에서 따로 정의 안 함)의 콜백함수를 변수 안에 넣어주기
+    var _url = request.url; //이름 다른 건 url 모듈과 변수명 구별 위해
+    var queryData = url.parse(_url, true).query;
+    console.log(queryData.id);
+    if(_url == '/'){
+      _url = '/index.html';
     }
-    if(request.url == '/favicon.ico'){
-        response.writeHead(404);
-        response.end();
-        return;
+    if(_url == '/favicon.ico'){
+        return response.writeHead(404);
     }
     response.writeHead(200);
-    console.log(__dirname + url); // 디렉토리와 파일 url을 띄워줌
-    response.end(fs.readFileSync(__dirname + url)); //response.end 안에 있는 건 특정 디렉토리의 url을 불러오게 하는 명령어
+    response.end(fs.readFileSync(__dirname + _url)); //response.end 안에 있는 건 특정 디렉토리의 url을 불러오게 하는 명령어
  
 });
 app.listen(3000);
