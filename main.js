cb1ea5f5e7c69e9c0b3abe7e5c6612606e6d99a3
SnowLeopardMoonBear <43803127+SnowLeopardMoonBear@users.noreply.github.com>
2020-04-12 18:39:23 +0900

    add console msg, const PORT

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url'); //node.js에서 url 모듈을 사용, 앞으로 url이란 변수로 접근해 사용.
-
+const PORT = 3001;
 
 var app = http.createServer(function(request,response){ //익명(위에서 따로 정의 안 함)의 콜백함수를 변수 안에 넣어주기
     var _url = request.url; //이름 다른 건 url 모듈과 변수명 구별 위해
@@ -17,4 +17,5 @@ var app = http.createServer(function(request,response){ //익명(위에서 따�
     response.end(fs.readFileSync(__dirname + _url)); //response.end 안에 있는 건 특정 디렉토리의 url을 불러오게 하는 명령어
  
 });
-app.listen(3000);
+app.listen(PORT,()=>console.log(`server running at port:${PORT}`));
+// 아직 에러 처리가 없어서 이상한 루트로 접근하면 바로 서버가 닫힙니다.
